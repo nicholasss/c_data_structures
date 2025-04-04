ffc45b6648abcbd564eae89f406c717e34170e70
Nicholas <nicholasss@users.noreply.github.com>
2025-04-04 12:41:14 -0400

    updated test

--- test_unsorted_array.c
+++ test_unsorted_array.c
@@ -3,19 +3,29 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+  // testing unsorted array with 10
   size_t capacity = (size_t)10;
   uArray *array = initUArray(capacity);
 
-  bool success = appendUArray(array, 1);
-  if (!success) {
-    printf("failure.\n");
-  } else {
-    printf("appended successfuly.\n");
+  // adding ten items
+  for (int i = 0; i < 10; i++) {
+    bool ok = appendUArray(array, i + 1);
+
+    if (!ok) {
+      printf("ERROR Unable to append to array.\n");
+      return EXIT_FAILURE;
+    }
   }
 
+  // prints whole array
   dumpUArray(array);
 
-  int num = popUArray(array);
+  // removing ten items
+  for (int i = 0; i < 10; i++) {
+    int num = popUArray(array);
+    printf("num popped: %d\n", num);
+  }
 
   freeUArray(array);
 
